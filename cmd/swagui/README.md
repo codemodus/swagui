3d3d74c1d12a7959f4543149db9a87027f48acdc
Daved <daved@codemodus.com>
2019-08-20 20:13:47 -0700

    Update cmd docs

--- cmd/swagui/README.md
+++ cmd/swagui/README.md
@@ -1,14 +1,16 @@
 # swagui
 
-    go get -u github.com/codemodus/swagui/cmd/swagui
+    go get github.com/codemodus/swagui/cmd/swagui
 
 swagui is a basic implementation of the swagui library which simplifies serving 
 an instance of Swagger-UI.
 
     Usage of swagui:
       -def string
-            default definition
+            remote definition
       -port string
             http port (default ":2288")
+      -scrape string
+            definition to scrape and serve
       -v int
             swagger version
--- cmd/swagui/doc.go
+++ cmd/swagui/doc.go
@@ -1,9 +1,13 @@
 // swagui is a basic implementation of the swagui library which simplifies
 // serving an instance of Swagger-UI.
 //
-//  Available flags:
-//
-//  --port={:1234}      Set the http port.         // ":2288"
-//  --path={path}       Set the path prefix.       // ""
-//  -v {version}        Set the Swagger version.   // "{current}"
+//  Usage of swagui:
+//    -def string
+//          remote definition
+//    -port string
+//          http port (default ":2288")
+//    -scrape string
+//          definition to scrape and serve
+//    -v int
+//          swagger version
 package main
